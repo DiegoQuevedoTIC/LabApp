d482611d8e42b48dc36ead4334b5a5599345808c
Diego Andres <diego01andres@hotmail.com>
2025-06-09 10:04:41 -0500

    Ajuste Usuario Administrador V1

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
     ];
 
         public function canAccessPanel(Panel $panel): bool
@@ -52,6 +53,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
         ];
     }
 
