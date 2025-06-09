841c95ca9560fbfc506adc5c6c9f0fdd335d44a9
Diego Andres <diego01andres@hotmail.com>
2025-06-09 09:54:04 -0500

    Ajuste Usuario Administrador

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,8 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -23,6 +25,12 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+        public function canAccessPanel(Panel $panel): bool
+    {
+        // Solo los usuarios con la columna is_admin = true podrán acceder.
+        return $this->is_admin;
+    }
 
     /**
      * The attributes that should be hidden for serialization.
