ddf395353e3f4ec0864919c47b5f24ee8b78fc00
Diego Andres <diego01andres@hotmail.com>
2025-06-12 23:08:05 -0500

    Ajuste Formulario Solicitudes

--- app/Filament/Resources/SolicitudResource.php
+++ app/Filament/Resources/SolicitudResource.php
@@ -15,11 +15,10 @@ use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\Select; // <-- Importante: Añadir Section
-use Filament\Forms\Components\View;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\BadgeColumn;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\FileUpload;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Estado;
@@ -43,24 +42,21 @@ public static function form(Form $form): Form
                 ->icon('heroicon-o-bookmark-square')
                 ->schema([
                     Grid::make(5)->schema([
-                            TextInput::make('id_solicitud')
-                                ->disabled()
-                                ->prefixIcon('heroicon-o-finger-print')
+                            TextInput::make('id')
                                 ->prefix('SOL-')
+                                ->label('ID de Solicitud') // Es mejor poner el label aquí
+                                ->readOnly() // 2. Usa readOnly para que el valor sea visible pero no editable
+                                ->extraInputAttributes(['style' => 'text-align: center;  background-color: teal; color: white; font-weight: bold;'])
+                                ->prefixIcon('heroicon-o-key')
                                 ->prefixIconColor('success')
-                                ->required()
-                                ->hint('ID de Solicitud')
-                                ->hintColor('primary')
-                                ->label(' ')
-                                ->disabled(fn (string $operation): bool => $operation === 'create' OR $operation === 'edit')
-                                ->helperText('ID Unico de Solicitud. Permite identificacion inequivoca.'),
+                                ->helperText('ID único de la solicitud.'),
                             Select::make('estado_id')
-                                ->relationship('estado', 'nombre')
                                 ->columnSpan(2)
+                                ->disabled()
+                                ->relationship('estado', 'nombre')
                                 ->hint('Estado de la Solicitud')
                                 ->hintColor('primary')
-                                ->default(fn () => Estado::where('nombre', 'Solicitada')->first()?->id)
-                                ->disabled(fn (string $operation): bool => $operation === 'create' OR $operation === 'edit')
+                                ->default(fn () => Estado::where('nombre', 'En revisión')->first()?->id)
                                 ->required()
                                 ->label(' ')
                                 ->helperText('Estado actual de la solictud.'),
@@ -68,6 +64,8 @@ public static function form(Form $form): Form
                                 ->default(now())
                                 ->required()
                                 ->hint('Fecha de Solicitud')
+                                ->required()
+
                                 ->hintColor('primary')
                                 ->columnSpan(2)
                                 ->disabled(fn (string $operation): bool =>  $operation === 'edit')
@@ -80,46 +78,68 @@ public static function form(Form $form): Form
                     ->description('Información principal y origen de la solicitud.')
                     ->icon('heroicon-o-clipboard-document-list')
                     ->schema([
-                        Section::make('Información de la Solicitud')->schema([
                             ToggleButtons::make('tipo_solicitud')
                                 ->label('Tipo de Solicitud')
                                 ->options([
                                     'Interna' => 'Interna',
                                     'Externa' => 'Externa',
                                 ])
-                                ->required()
+
+                                ->default('Interna')
+                                ->helperText('Seleccione el tipo de solicitud.')
                                 ->inline()
                                 ->live(),
-                            TextInput::make('referencia')
-                                ->required()
-                                ->label('Referencia')
-                                ->maxLength(64)
-                                ->unique(ignoreRecord: true)
-                                ->columnSpan(2),
-                        ])->columns(5),
                         Section::make('Origen y Ubicación')
                         ->visible(fn (Get $get): bool => $get('tipo_solicitud') === 'Interna')
                         ->schema([
                             Select::make('direccion_tecnica_id')
                                 ->relationship('direccionTecnica', 'nombre')
+                                ->required()
+                                ->validationMessages([
+                                    'required' => 'Por favor, selecciona una dirección técnica.',
+                                ])
                                 ->label('Dirección Técnica'),
                             Select::make('grupo_trabajo_id')
                                 ->relationship('grupoTrabajo', 'nombre')
+                                ->required()
+                                ->validationMessages([
+                                    'required' => 'Por favor, selecciona un grupo de trabajo.',
+                                ])
                                 ->label('Grupo de Trabajo'),
                             Select::make('proyecto_id')
                                 ->relationship('proyecto', 'nombre')
+                                ->required()
+                                ->validationMessages([
+                                    'required' => 'Por favor, selecciona un proyecto.',
+                                ])
                                 ->label('Proyecto'),
                                 ])
                             ->columns(3),
                         Section::make('Origen y Ubicación')
                         ->visible(fn (Get $get): bool => $get('tipo_solicitud') === 'Externa')
                         ->schema([
+                            TextInput::make('referencia')
+                                ->required()
+                                ->label('Radicado de Solicitud')
+                                ->maxLength(64)
+                                ->validationMessages([
+                                        'required' => 'Por favor, ingresa el codigo del radicado con que se recibio la solicitud.',
+                                    ])
+                                ->unique(ignoreRecord: true)
+                                ->columnSpan(1),
                             TextInput::make('entidad')
                                 ->label('Entidad Solicitante')
+                                ->columnSpan(1)
+                                ->validationMessages([
+                                        'required' => 'Por favor, ingresa el nombre de la entidad que hace la solicitud.',
+                                    ])
                                 ->required(),
                             TextInput::make('direccion_ciudad')
                                 ->label('Dirección - Ciudad de Origen')
-                                ->columnSpanFull()
+                                ->columnSpan(1)
+                                ->validationMessages([
+                                    'required' => 'Por favor, ingresa la dirección y ciudad de origen de la solicitud.',
+                                ])
                                 ->required(),
                                 ])
                             ->columns(3)
@@ -135,16 +155,28 @@ public static function form(Form $form): Form
                             ->multiple()
                             ->preload()
                             ->searchable()
+                            ->required()
+                            ->validationMessages([
+                                'required' => 'Es necesario incluir por lo menos un ensayo.',
+                                    ])
                             ->columnSpanFull()
-                            ->label('Ensayos Solicitados')
                             ->label('Ensayos o Actividades Solicitadas'),
                         RichEditor::make('requisitos_especiales')
+                            ->label('Requisitos Especiales')
+                            ->required()
+                            ->validationMessages([
+                                'required' => 'Por favor, especifica los requisitos especiales o condiciones para los ensayos.',
+                            ])
+                            ->placeholder('Especificar requisitos especiales, condiciones o especificaciones para los ensayos.')
                             ->columnSpanFull(),
                         TextInput::make('cantidad_muestras_proyectadas')
                             ->numeric()
                             ->label('Cantidad de Muestras Proyectadas')
-                            ->columns(1)
-                            ->required(),
+                            ->required()
+                            ->validationMessages([
+                                'required' => 'Ingresa la cantidad de muestras proyectadas.',
+                            ])
+                            ->columns(1),
                         FileUpload::make('soporte')
                             ->disk('public')
                             ->label('Soporte de Solicitud')
@@ -159,38 +191,42 @@ public static function form(Form $form): Form
                     ->description('Personas responsables de la solicitud.')
                     ->icon('heroicon-o-users')
                     ->schema([
-                        Grid::make(2)->schema([
-                            Section::make('Contacto 1: Reporte de Resultados')
-                                ->schema([
-                                    Grid::make(2)->schema([
-                                        TextInput::make('contacto_1_nombre')->label('Nombre')->required()->columnSpan(2),
+                        Grid::make(6)->schema([
+                                        TextInput::make('contacto_1_nombre')
+                                            ->label('Nombre')
+                                            ->required()
+                                            ->hint('Se necesita por lo menos registrar un contacto.')
+                                            ->validationMessages([
+                                            'required' => 'Por favor, ingresa el nombre del contacto para el reporte de resultados.',
+                                            ])
+                                            ->columnSpan(3),
                                         TextInput::make('contacto_1_extension')
                                             ->columnSpan(1)
-                                            ->label('Teléfono / Extensión'),
-                                    ]),
-                                    TextInput::make('contacto_1_email')
-                                        ->label('Correo Electrónico')
-                                        ->email()
-                                        ->columnSpan(2)
-                                        ->required(),
-                                ]),
-                            Section::make('Contacto 2: Entrega de Muestras')
-                                ->schema([
-                                    Grid::make(5)->schema([
+                                            ->required()
+                                            ->validationMessages([
+                                                'required' => 'Ingresa el teléfono.',
+                                            ])
+                                            ->label('Teléfono'),
+                                        TextInput::make('contacto_1_email')
+                                            ->label('Correo Electrónico')
+                                            ->email()
+                                            ->columnSpan(2)
+                                            ->validationMessages([
+                                                'required' => 'Ingresa el mail del contacto para la gestion de la solicitud.',
+                                            ])
+                                            ->required(),
                                         TextInput::make('contacto_2_nombre')
                                             ->label('Nombre')
-                                            ->columnSpan(2)
-                                            ->required(),
+                                            ->columnSpan(3),
                                         TextInput::make('contacto_2_extension')
                                         ->label('Teléfono / Extensión')
                                         ->columnSpan(1),
-                                    ]),
                                     TextInput::make('contacto_2_email')
                                         ->label('Correo Electrónico')
                                         ->columnSpan(2)
                                         ->email()
-                                        ->required(),
-                                ]),
+
+
                         ])
                     ]),
             ])->columnSpanFull(),
@@ -201,7 +237,10 @@ public static function form(Form $form): Form
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('referencia')
+                Tables\Columns\TextColumn::make('id')
+                    ->label('ID ')
+                    ->formatStateUsing(fn (string $state): string => "SOL-" . str_pad($state, 0, '0', STR_PAD_LEFT))
+                    ->sortable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('tipo_solicitud')
                     ->searchable(),
@@ -211,22 +250,13 @@ public static function form(Form $form): Form
                 Tables\Columns\TextColumn::make('estado.nombre')
                     ->numeric()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('entidad')
-                    ->label('Entidad Solicitante')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('cantidad_muestras_proyectadas')
-                    ->numeric()
-                    ->label('Muestras Proyectadas')
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
+                Tables\Columns\TextColumn::make('ensayos.descripcion')
+                    ->label('Ensayos Solicitados')
+                    ->listWithLineBreaks()
+                    ->badge()
+                    ->limit(80)
                     ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    ->searchable(),
             ])
             ->filters([
                 //
